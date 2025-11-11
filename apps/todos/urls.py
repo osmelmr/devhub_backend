1b5a5d495301b30a1d5d73626bf0c3abe73ed057
Osmel <osmelmr.dev@gmail.com>
2025-11-11 00:08:21 -0500

    toodos

--- apps/todos/urls.py
+++ apps/todos/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('todos/', views.list_todos, name='list_todos'),
-    path('todos/search/', views.search_todos, name='search_todos'),
-    path('todos/<int:pk>/', views.retrieve_todo, name='retrieve_todo'),
-    path('todos/create/', views.create_todo, name='create_todo'),
-    path('todos/<int:pk>/update/', views.update_todo, name='update_todo'),
-    path('todos/<int:pk>/delete/', views.delete_todo, name='delete_todo'),
-    path('todos/bulk-delete/', views.bulk_delete_todos, name='bulk_delete_todos'),
+    path('', views.list_todos, name='list_todos'),
+    path('search/', views.search_todos, name='search_todos'),
+    path('<int:pk>/', views.retrieve_todo, name='retrieve_todo'),
+    path('create/', views.create_todo, name='create_todo'),
+    path('<int:pk>/update/', views.update_todo, name='update_todo'),
+    path('<int:pk>/delete/', views.delete_todo, name='delete_todo'),
+    path('bulk-delete/', views.bulk_delete_todos, name='bulk_delete_todos'),
 ]
