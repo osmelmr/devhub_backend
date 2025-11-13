cecc3f22c47053a2f6ed506f8b86de57546556d2
Osmel <osmelmr.dev@gmail.com>
2025-11-13 13:12:48 -0500

    rename fields todo model

--- apps/todos/models.py
+++ apps/todos/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-    title = models.CharField(max_length=255)
-    completed = models.BooleanField(default=False)
+    text = models.CharField(max_length=255)
+    done = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
