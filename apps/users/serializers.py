b582e69a710865db7e4fbc63ef4d84474fd6ae1d
Osmel <osmelmr.dev@gmail.com>
2025-11-27 05:34:04 -0500

    ejecutando migraciones

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from .models import User
-from apps.projects.models import Project  # Asegúrate de que la ruta sea correcta
+from apps.projects.models import Project
 
 class UserSerializer(serializers.ModelSerializer):
     projects = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
+    
     class Meta:
         model = User
         fields = [
@@ -14,6 +14,27 @@ class UserSerializer(serializers.ModelSerializer):
             'first_name',
             'last_name',
             'role',
-            'avatar_url', 
+            'avatar_url',
             'projects',
+            'password',
         ]
+        extra_kwargs = {
+            'password': {'write_only': True}  # Nunca devolver la contraseña
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)  # Hash automático
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)  # Hash automático
+        instance.save()
+        return instance
